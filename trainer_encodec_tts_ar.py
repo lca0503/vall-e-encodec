2e10097ada95b4a4776e454b4567f3766dd2f701
lca0503 <puredzom1234@gmail.com>
2023-04-26 15:17:27 +0800

    Debug and change some training config of ar tts

--- trainer_encodec_tts_ar.py
+++ trainer_encodec_tts_ar.py
@@ -1,56 +1,52 @@
-import wandb
 from datasets import load_dataset
 from jiwer import wer
 from transformers import (AutoTokenizer, BartForConditionalGeneration,
                           DataCollatorForSeq2Seq, Seq2SeqTrainer,
-                          Seq2SeqTrainingArguments)
+                          Seq2SeqTrainingArguments, GenerationConfig)
 
-name = 'libri960_ar_wr0.08_lr3e-5'
-wandb.init(name)
+import wandb
+
 
 # Load dataset and tokenizer
-dataset = load_dataset("voidful/librispeech_encodec")
+train_dataset = load_dataset("voidful/librispeech_encodec", split="trainclean100+trainclean360+trainother500")
+valid_dataset = load_dataset("voidful/librispeech_encodec", split="validationclean")
 tokenizer = AutoTokenizer.from_pretrained("voidful/bart-base-unit")
+
+# Load model
 model = BartForConditionalGeneration.from_pretrained("voidful/bart-base-unit")
 
-# Split the dataset into training and validation sets
-
-train_dataset = load_dataset(
-    "voidful/librispeech_encodec",
-    "train",
-    split="trainclean100+trainclean360+trainother500")
-# valid_dataset = dataset['validationclean']
-valid_dataset = dataset['validationclean']
-
+    
 # Set training parameters
 training_args = Seq2SeqTrainingArguments(
     output_dir="./training_output",
-    num_train_epochs=50,
+    num_train_epochs=10,
     per_device_train_batch_size=2,
     per_device_eval_batch_size=2,
     warmup_ratio=0.08,
-    weight_decay=0.01,
+    weight_decay=1e-4,
     logging_dir="./logs",
     logging_steps=500,
-    save_steps=50000,
+    save_steps=2000,
     save_total_limit=2,
     evaluation_strategy="steps",
-    eval_steps=50000,
+    eval_steps=2000,
     predict_with_generate=True,
     fp16=True,
     gradient_accumulation_steps=8,
-    learning_rate=3e-5,
+    learning_rate=1e-4,
+    generation_max_length=1024,
 )
 
-# Define a data collator to handle tokenization
-data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
+# Filter long examples
+def filter_examples(example):
+    return len(example[f"encodec_0"]) <= 1000
 
+train_dataset = train_dataset.filter(filter_examples)
+valid_dataset = valid_dataset.filter(filter_examples)
 
 def pad_sequences(sequences, max_length, padding_value):
     return [sequence + [padding_value] * (max_length - len(sequence)) for sequence in sequences]
 
-
-# Define training and validation functions
 def process_data_to_model_inputs(batch):
     input_ids = []
     attention_mask = []
@@ -69,18 +65,6 @@ def process_data_to_model_inputs(batch):
         attention_mask.append(data['attention_mask'])
         decoder_input_ids.append(decoder_input_id)
         labels.append(label)
-        
-        '''
-        # 1-7 layer NAR data
-        for i in range(1, 8):
-            decoder_input_id = tokenizer.convert_tokens_to_ids(
-                [f"v_tok_{u + (i - 1) * 1000}" for u in batch[f'encodec_{i - 1}'][b]])
-            label = tokenizer.convert_tokens_to_ids([f"v_tok_{u + i * 1000}" for u in batch[f'encodec_{i}'][b]])
-            input_ids.append(data['input_ids'])
-            attention_mask.append(data['attention_mask'])
-            decoder_input_ids.append(decoder_input_id)
-            labels.append(label)
-        '''
 
     # Pad decoder_input_ids and labels
     decoder_input_ids = pad_sequences(decoder_input_ids, max_length=max_length, padding_value=tokenizer.pad_token_id)
@@ -93,27 +77,23 @@ def process_data_to_model_inputs(batch):
         "labels": labels
     }
 
-
-def filter_examples(example):
-    return len(example[f"encodec_0"]) <= 1000
-
-
-train_dataset = train_dataset.filter(filter_examples)
-valid_dataset = valid_dataset.filter(filter_examples)
-
 train_dataset = train_dataset.map(
     process_data_to_model_inputs,
     remove_columns=train_dataset.column_names,
     batched=True,
     batch_size=training_args.per_device_train_batch_size
 )
-valid_dataset = valid_dataset.map(process_data_to_model_inputs,
-                                  remove_columns=valid_dataset.column_names,
-                                  batched=True,
-                                  batch_size=training_args.per_device_eval_batch_size
-                                  )
+valid_dataset = valid_dataset.map(
+    process_data_to_model_inputs,
+    remove_columns=valid_dataset.column_names,
+    batched=True,
+    batch_size=training_args.per_device_eval_batch_size
+)
 
+# Define a data collator to handle tokenization
+data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
 
+# Define metrics
 def compute_metrics(eval_pred):
     predictions, labels = eval_pred
     labels = [i[i != -100] for i in labels]
@@ -121,14 +101,19 @@ def compute_metrics(eval_pred):
     decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
 
     # Compute WER
-    wer_value = wer(decoded_labels, decoded_preds)
+    wer_value = wer([" ".join(filter(None, i.split("v_tok_"))) for i in decoded_labels],
+                    [" ".join(filter(None, i.split("v_tok_"))) for i in decoded_preds])
     print("pred_result")
     print("=================================")
     for i in range(10):
-        print(decoded_labels[i], " ///// ", decoded_preds[i])
+        print("target:", labels[i])
+        print("pred:", predictions[i])
+        print("-----------------")
     print("=================================")
     return {"wer": wer_value}
 
+name = 'libri960_ar_wr0.08_lr1e-4'
+wandb.init(name)
 
 # Create the trainer
 trainer = Seq2SeqTrainer(
@@ -138,7 +123,7 @@ trainer = Seq2SeqTrainer(
     eval_dataset=valid_dataset,
     data_collator=data_collator,
     tokenizer=tokenizer,
-	compute_metrics=compute_metrics,
+    compute_metrics=compute_metrics,
 )
 
 # Start training
