c6dc413fc9cc6c418503f2e42733bc16663d09be
lca0503 <puredzom1234@gmail.com>
2023-05-03 17:12:52 +0800

    Fix get_transcriptions.py bugs

--- sox_data/get_transcriptions.py
+++ sox_data/get_transcriptions.py
@@ -7,21 +7,22 @@ from tqdm import tqdm
 
 
 def main(args):
-    todo = list(Path(args.data_dir).rglob("*.trans.tsv"))
+    todo = list(Path(args.data_dir).rglob("*.original.txt"))
     path_to_transcription = defaultdict()
     
-    for transcription_file in tqdm(todo, desc="Loading", ascii=False, ncols=100):
-        transcription_dir = os.path.dirname(transcription_file)
-        with open(transcription_file, 'r') as f:
+    for transcription_path in tqdm(todo, desc="Loading", ascii=False, ncols=100):
+        transcription_dir = os.path.dirname(transcription_path)
+        speech_id = str(os.path.basename(transcription_path)).split('.')[0]
+        speech_path = f"{transcription_dir}/{speech_id}.wav"        
+        assert os.path.isfile(speech_path)
+        with open(transcription_path, 'r') as f:
+            transcription = ""
             content = f.readlines()
             for line in content:
-                line = line.split('\t')
-                speech_id = line[0]
-                speech_path = f"{transcription_dir}/{speech_id}.wav"
-                transcription = " ".join(line[1:])
+                transcription += str(line)
                 path_to_transcription[speech_path] = transcription
                 
-    with open(args.output_file, 'w') as f:
+    with open(args.output_path, 'w') as f:
         for speech_path, transcription in path_to_transcription.items():
             f.write("\t".join([speech_path, transcription]))
             
@@ -29,7 +30,7 @@ def main(args):
 def parse_args() -> Namespace:
     parser = ArgumentParser()
     parser.add_argument("-d", "--data_dir", type=str, default="./data/libritts")
-    parser.add_argument("-o", "--output_file", type=str, default="./data/libritts_transcriptions.tsv")
+    parser.add_argument("-o", "--output_path", type=str, default="./data/libritts_transcriptions.tsv")
     
     args = parser.parse_args()
     return args
