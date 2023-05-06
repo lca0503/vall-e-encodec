554ec6e56c16db36537002e40a99aafed1d834c0
lca0503 <puredzom1234@gmail.com>
2023-05-06 23:05:49 +0800

    Add NAR EnCodec BART

---
+++ nar_encodec_bart.py
@@ -0,0 +1,57 @@
+# coding=utf-8
+# Copyright 2021 The Fairseq Authors and The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+""" PyTorch BART model."""
+
+from transformers.models.bart.modeling_bart import *
+from encodec_bart_model import BartEncodecEncoder
+from encodec_tts.nar_bart import NARBartDecoder
+
+
+class NARBartEncodecModel(BartModel):
+    _keys_to_ignore_on_load_missing = ["encoder.embed_tokens.weight", "decoder.embed_tokens.weight"]
+
+    def __init__(self, config: BartConfig):
+        super().__init__(config)
+
+        padding_idx, vocab_size = config.pad_token_id, config.vocab_size
+        self.shared = nn.Embedding(vocab_size, config.d_model, padding_idx)
+
+        # Modified: BartEncoder() to BartEncodecEncoder()
+        self.encoder = BartEncodecEncoder(config, self.shared)
+        # Modified: BartDecoder() to NARBartDecoder()
+        self.decoder = NARBartDecoder(config, self.shared)
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+
+class NARBartEncodecForConditionalGeneration(BartForConditionalGeneration):
+    base_model_prefix = "model"
+    _keys_to_ignore_on_load_missing = [
+        r"final_logits_bias",
+        r"lm_head.weight",
+        "encoder.embed_tokens.weight",
+        "decoder.embed_tokens.weight",
+    ]
+
+    def __init__(self, config: BartConfig):
+        super().__init__(config)
+        # Modified: BartModel() to NARBartEncodecModel()
+        self.model = NARBartEncodecModel(config)
+        self.register_buffer("final_logits_bias", torch.zeros((1, self.model.shared.num_embeddings)))
+        self.lm_head = nn.Linear(config.d_model, self.model.shared.num_embeddings, bias=False)
+
+        # Initialize weights and apply final processing
+        self.post_init()
