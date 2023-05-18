5cba23ddc6bb5effbc01e7ffa2d8a757dc78b56e
lca0503 <puredzom1234@gmail.com>
2023-05-18 18:36:53 +0800

    Add decoder attention mask

--- trainer_encodec_tts_nar.py
+++ trainer_encodec_tts_nar.py
@@ -10,7 +10,7 @@ from transformers import (AutoTokenizer, Seq2SeqTrainer,
 from encodec_model.nar_bart_model import NARBartForConditionalGeneration
 
 wandb.init(project="encodec_tts", 
-           name="bart-base-nar",
+           name="bart-base-nar-fixed",
 )
 
 
@@ -35,8 +35,10 @@ TRAIN_ARGS = Seq2SeqTrainingArguments(
 )
 
 
-def pad_sequences(sequences, max_length, padding_value):
-    return [sequence + [padding_value] * (max_length - len(sequence)) for sequence in sequences]
+def pad_sequences_and_create_masks(sequences, max_length, padding_value):
+    padded_sequences = [sequence + [padding_value] * (max_length - len(sequence)) for sequence in sequences]
+    attention_masks = [[1 if token != padding_value else 0 for token in sequence] for sequence in padded_sequences]
+    return padded_sequences, attention_masks
 
 
 def filter_examples(example):
@@ -54,26 +56,29 @@ def process_data_to_model_inputs(batch, tokenizer):
     eos_token_id = tokenizer.eos_token_id
     sep_token_id = tokenizer.sep_token_id
     pad_token_id = tokenizer.pad_token_id
-    
+
     for b in range(len(batch["text"])):
         data = tokenizer(batch["text"][b], padding="max_length", truncation=True, max_length=max_length)
         for i in range(1, 8):
             decoder_input_id = tokenizer.convert_tokens_to_ids(
                 [f"v_tok_{u + (i - 1) * 1024}" for u in batch[f"encodec_{i - 1}"][b]])
             label = tokenizer.convert_tokens_to_ids([f"v_tok_{u + i * 1024}" for u in batch[f"encodec_{i}"][b]])
+            
             input_ids.append(data["input_ids"])
             attention_mask.append(data["attention_mask"])
             decoder_input_ids.append(decoder_input_id)
             labels.append(label)
 
     # Pad decoder_input_ids and labels
-    decoder_input_ids = pad_sequences(decoder_input_ids, max_length=max_length, padding_value=pad_token_id)
-    labels = pad_sequences(labels, max_length=max_length, padding_value=-100)
+    decoder_input_ids, decoder_attention_mask = pad_sequences_and_create_masks(decoder_input_ids, max_length=max_length,
+                                                                               padding_value=pad_token_id)
+    labels, _ = pad_sequences_and_create_masks(labels, max_length=max_length, padding_value=-100)
 
     return {
         "input_ids": input_ids,
         "attention_mask": attention_mask,
         "decoder_input_ids": decoder_input_ids,
+        "decoder_attention_mask": decoder_attention_mask,
         "labels": labels
     }
 
@@ -103,6 +108,7 @@ def get_dataset(tokenizer, args):
     return train_dataset, eval_dataset
 
 
+# Only compute raw output (Inference code will check unit range)
 def preprocess_logits_for_metrics(ret, labels):
     logits, encoder_last_hidden_state = ret
     predictions = torch.argmax(logits, axis=-1)
