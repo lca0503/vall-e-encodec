91b4b6419ce9cd8d8052cff3c61389e56fe9c76f
lca0503 <puredzom1234@gmail.com>
2023-05-31 17:23:33 +0800

    Modify encodec vc ar codes

--- trainer_encodec_vc_ar.py
+++ trainer_encodec_vc_ar.py
@@ -5,59 +5,48 @@ from datasets import load_dataset
 from jiwer import wer
 from transformers import (AutoTokenizer, BartForConditionalGeneration,
                           Seq2SeqTrainer, Seq2SeqTrainingArguments)
+import os
+
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
 
 wandb.init(project="encodec_vc", 
-           name="bart-base-ar",
+           name="speech-chatpgpt-base-ar",
 )
 
 
 TRAIN_ARGS = Seq2SeqTrainingArguments(
-    output_dir="./training_output/vc_ar",
-    num_train_epochs=10,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
+    output_dir="./training_output/speech-chatpgpt-base-ar",
+    num_train_epochs=5,
+    per_device_train_batch_size=6,
+    per_device_eval_batch_size=6,
     warmup_ratio=0.08,
-    weight_decay=1e-4,
-    logging_dir="./logs/vc_ar",
+    weight_decay=1e-2,
+    logging_dir="./logs/speech-chatpgpt-base-ar",
     logging_steps=500,
-    save_steps=5000,
-    save_total_limit=2,
+    save_steps=10000,
+    save_total_limit=5,
     evaluation_strategy="steps",
-    eval_steps=5000,
+    eval_steps=10000,
     predict_with_generate=True,
     fp16=True,
-    gradient_accumulation_steps=2,
-    learning_rate=1e-4,
+    learning_rate=1e-5,
     generation_max_length=1024,
+    push_to_hub=True,
+    hub_model_id="lca0503/speech-chatpgpt-base-ar",
     report_to="wandb",
 )
 
 
-def pad_sequences(sequences, max_length, padding_value):
-    return [sequence + [padding_value] * (max_length - len(sequence)) for sequence in sequences]
-
-
-def get_attention_mask(seq_len, max_length):
-    return [1] * seq_len + [0] * (max_length - seq_len)
-
-
-def filter_examples(example):
-    return len(example[f"src_encodec_0"]) <= 800
-
-
-def get_encodec_units(data, b, split="src"):
-    encodec_units = [
-        [f"v_tok_{u + l * 1024}" for u in data[f"{split}_encodec_{l}"][b]]
-        for l in range(8)
-    ]
-    for e in encodec_units:
-        assert len(e) == len(encodec_units[0]), f"Inconsistent encodec unit length"
-    return encodec_units
+def pad_sequences_and_create_masks(sequences, max_length, padding_value):
+    padded_sequences = [sequence + [padding_value] * (max_length - len(sequence)) for sequence in sequences]
+    attention_masks = [[1 if token != padding_value else 0 for token in sequence] for sequence in padded_sequences]
+    return padded_sequences, attention_masks
 
 
 def process_data_to_model_inputs(batch, tokenizer):
     input_ids = []
-    attention_masks = []
     decoder_input_ids = []
     labels = []
 
@@ -68,40 +57,39 @@ def process_data_to_model_inputs(batch, tokenizer):
     pad_token_id = tokenizer.pad_token_id
 
     for b in range(len(batch["instruction"])):
-        # encoder input
         instruction_ids = tokenizer(batch["instruction"][b])["input_ids"][1 : -1]
         transcription_ids = tokenizer(batch["transcription"][b])["input_ids"][1 : -1]
-        encodec_layer0 = tokenizer.convert_tokens_to_ids(
-                [f"v_tok_{u}" for u in batch[f"src_encodec_{0}"][b]])
-        # encodec_layer0 = []
-        encoder_input_id = [bos_token_id] + \
-                        instruction_ids + [sep_token_id] + \
-                        transcription_ids + [sep_token_id] + \
-                        encodec_layer0 + [eos_token_id]
-        seq_len = len(encoder_input_id)
-        attention_mask = get_attention_mask(seq_len, max_length)
 
-        # decoder input
-        encode_input = tokenizer.convert_tokens_to_ids(
-            [f"v_tok_{u}" for u in batch[f"tgt_encodec_{0}"][b]])
-        decoder_input_id = [bos_token_id] + encode_input
-        label = encode_input + [eos_token_id]
+        # Decoder input
+        curr_tgt_encodec_ids = tokenizer.convert_tokens_to_ids(
+            [f"v_tok_{u}" for u in batch["tgt_encodec_0"][b]])
+        
+        # Encoder input
+        curr_src_encodec_ids = tokenizer.convert_tokens_to_ids(
+            [f"v_tok_{u}" for u in batch["src_encodec_0"][b]])
+        encoder_input_ids = [bos_token_id] + \
+            instruction_ids + [sep_token_id] + \
+            transcription_ids + [sep_token_id] + \
+            curr_src_encodec_ids + [eos_token_id]
 
-        input_ids.append(encoder_input_id)
-        attention_masks.append(attention_mask)
-        decoder_input_ids.append(decoder_input_id)
-        labels.append(label)
+        # Filter inputs
+        if len(encoder_input_ids) > max_length or len(curr_tgt_encodec_ids) + 1 > max_length:
+            continue
 
-    input_ids = pad_sequences(input_ids, max_length=max_length,
-                              padding_value=pad_token_id)
-    decoder_input_ids = pad_sequences(decoder_input_ids, max_length=max_length,
-                                      padding_value=pad_token_id)
-    labels = pad_sequences(labels, max_length=max_length,
-                           padding_value=pad_token_id)
+        input_ids.append(encoder_input_ids)
+        decoder_input_ids.append([bos_token_id] + curr_tgt_encodec_ids)
+        labels.append(curr_tgt_encodec_ids + [eos_token_id])
+
+    input_ids, attention_mask = pad_sequences_and_create_masks(input_ids, max_length=max_length,
+                                                               padding_value=pad_token_id)
+    decoder_input_ids, _ = pad_sequences_and_create_masks(decoder_input_ids, max_length=max_length,
+                                                          padding_value=pad_token_id)
+    labels, _ = pad_sequences_and_create_masks(labels, max_length=max_length,
+                                               padding_value=-100)
 
     return {
         "input_ids": input_ids,
-        "attention_mask": attention_masks,
+        "attention_mask": attention_mask,
         "decoder_input_ids": decoder_input_ids,
         "labels": labels
     }
@@ -110,9 +98,6 @@ def process_data_to_model_inputs(batch, tokenizer):
 def get_dataset(tokenizer, args):
     train_dataset = load_dataset(args.dataset, "train", split="+".join(args.train_splits))
     eval_dataset = load_dataset(args.dataset, "eval", split="+".join(args.eval_splits))
-
-    train_dataset = train_dataset.filter(filter_examples)
-    eval_dataset = eval_dataset.filter(filter_examples)
 
     train_dataset = train_dataset.map(
         process_data_to_model_inputs,
@@ -135,6 +120,7 @@ def get_dataset(tokenizer, args):
 def compute_metrics(eval_pred, tokenizer):
     predictions, labels = eval_pred
     labels = [i[i != -100] for i in labels]
+
     decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
     decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
 
@@ -173,10 +159,10 @@ def main(args):
 
 def parse_args() -> Namespace:
     parser = ArgumentParser()
-    parser.add_argument("-d", "--dataset", type=str, default="lca0503/soxdata_small_encodec")
+    parser.add_argument("-d", "--dataset", type=str, default="lca0503/GPTspeech_encodec")
     parser.add_argument("-t", "--train_splits", type=str, nargs="+", default=["train"])
     parser.add_argument("-e", "--eval_splits", type=str, nargs="+", default=["validation"])
-    parser.add_argument("-m", "--model_name", type=str, default="voidful/bart-base-unit")
+    parser.add_argument("-m", "--model_name", type=str, default="/work/b08902123/SpeechChatGPT/previous_ckpt/tts_ar/checkpoint-75000/")
 
     args = parser.parse_args()    
     return args
